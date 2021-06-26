66971f01de63bef961b392bd986e840862c08eb0
atul-git-cpu <67404388+atul-git-cpu@users.noreply.github.com>
2021-06-26 20:21:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> from flask import Flask, request, render_template
+from flask import Flask, request, render_template
 from flask_cors import cross_origin
 import sklearn
 import pickle
